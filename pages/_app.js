961c54d6911b9df6420dc73df4a71223f6eab9e4
ll782 <laurence.lord@gmail.com>
2020-07-21 22:10:37 +0100

    Code consistency

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <style jsx global>{`
-        body {
-          margin: 0;
-        }
-      `}</style>
-      <Component {...pageProps} />
-    </>
-  );
-}
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <style jsx global>{`
+      body {
+        margin: 0;
+      }
+    `}</style>
+    <Component {...pageProps} />
+  </>
+);
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,19 @@ import Head from "next/head";
 import AlignmentGenerator from "../components/AlignmentGenerator";
 import Main from "../components/Main";
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>LL782 Pathfinder Tools</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Main>
-        <AlignmentGenerator alignmentName="Lawful Good" />
-        <AlignmentGenerator alignmentName="Chaotic Good" />
-        <AlignmentGenerator alignmentName="Lawful Evil" />
-        <AlignmentGenerator alignmentName="Chaotic Evil" />
-      </Main>
-    </>
-  );
-}
+const Home = () => (
+  <>
+    <Head>
+      <title>LL782 Pathfinder Tools</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Main>
+      <AlignmentGenerator alignmentName="Lawful Good" />
+      <AlignmentGenerator alignmentName="Chaotic Good" />
+      <AlignmentGenerator alignmentName="Lawful Evil" />
+      <AlignmentGenerator alignmentName="Chaotic Evil" />
+    </Main>
+  </>
+);
+
+export default Home;
