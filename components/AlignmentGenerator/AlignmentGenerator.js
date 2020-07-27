f6467cce8123f1d51382324ee29800990815cb04
ll782 <laurence.lord@gmail.com>
2020-07-27 21:28:49 +0100

    Generate alignment ranges dynamically

--- components/AlignmentGenerator/AlignmentGenerator.js
+++ components/AlignmentGenerator/AlignmentGenerator.js
@@ -6,29 +6,64 @@ import {
 } from "../alignmentRelations";
 import toInitials from "../toInitials";
 
-const AlignmentGenerator = ({ alignmentName = "Lawful Good" }) => {
-  const original = [alignmentName];
-  const onceRemoved = oneStepRemovedFrom(alignmentName);
-  const twiceRemoved = twoStepsRemovedFrom(alignmentName);
-  const threeTimesRemoved = threeStepsRemovedFrom(alignmentName);
-  const fourTimesRemoved = fourStepsRemovedFrom(alignmentName);
+const numberOfAlignments = 9;
+const baseChance = (100 - numberOfAlignments) / numberOfAlignments;
+
+const addStepsRemoved = (stepsRemoved) => (alignment) => ({
+  alignment,
+  stepsRemoved,
+});
+
+const getMeanStepsRemoved = (alignments) => {
+  const stepsRemoved = alignments.map(({ stepsRemoved }) => stepsRemoved);
+  const sum = stepsRemoved.reduce((a, b) => (a || 0) + b);
+  return sum / alignments.length;
+};
+
+const formatAlignment = toInitials;
+const formatChance = (topOfRange, bottomOfRange) => {
+  topOfRange = topOfRange > 0 ? Math.round(topOfRange) : 0;
+  bottomOfRange = bottomOfRange > 1 ? Math.round(bottomOfRange) : 1;
+  if (topOfRange === 0) {
+    bottomOfRange = 0;
+  }
+  return topOfRange === bottomOfRange
+    ? `${topOfRange}%`
+    : `${bottomOfRange}-${topOfRange}%`;
+};
+
+const AlignmentGenerator = ({ alignment = "Lawful Good", dominance = 10 }) => {
+  const base = [{ alignment, stepsRemoved: 0 }];
+  const near = oneStepRemovedFrom(alignment).map(addStepsRemoved(1));
+  const mid = twoStepsRemovedFrom(alignment).map(addStepsRemoved(2));
+  const far = threeStepsRemovedFrom(alignment).map(addStepsRemoved(3));
+  const opposite = fourStepsRemovedFrom(alignment).map(addStepsRemoved(4));
+
+  const alignments = base.concat(near, mid, far, opposite);
+
+  const extremism = getMeanStepsRemoved(alignments);
+  const adheranceToTheBase = dominance / extremism;
+
+  const getDeviation = ({ stepsRemoved }) =>
+    (extremism - stepsRemoved) * adheranceToTheBase;
 
   let maxChance = 100;
 
-  const alignments = original.concat(
-    onceRemoved,
-    twiceRemoved,
-    threeTimesRemoved,
-    fourTimesRemoved
-  );
+  const alignmentChances = alignments.map((a) => {
+    const { alignment, stepsRemoved } = a;
+    const deviation = getDeviation({ stepsRemoved });
+    const chance = baseChance + deviation;
+    const topOfRange = maxChance;
+    const bottomOfRange = maxChance - chance;
 
-  const chances = [23, 16, 16, 11, 6, 6, 6, 6, 1];
+    const formattedResult = [
+      formatAlignment(alignment),
+      formatChance(topOfRange, bottomOfRange),
+    ];
 
-  const alignmentChances = alignments.map((alignmentName, i) => {
-    const bottom = maxChance - chances[i];
-    const top = maxChance;
-    maxChance = bottom - 1;
-    return [toInitials(alignmentName), `${bottom}-${top}%`];
+    maxChance = bottomOfRange - 1;
+
+    return formattedResult;
   });
 
   // TODO: perform a random role
@@ -36,7 +71,7 @@ const AlignmentGenerator = ({ alignmentName = "Lawful Good" }) => {
   return (
     <>
       <table>
-        <caption>{alignmentName} territory</caption>
+        <caption>{alignment} territory</caption>
         <thead>
           <tr>
             <th scope="col">Alignment</th>
@@ -44,9 +79,9 @@ const AlignmentGenerator = ({ alignmentName = "Lawful Good" }) => {
           </tr>
         </thead>
         <tbody>
-          {alignmentChances.map(([alignment, chance]) => (
-            <tr key={`${alignmentName}_${alignment}`}>
-              <th scope="row">{alignment}</th>
+          {alignmentChances.map(([a, chance]) => (
+            <tr key={`${alignment}_${a}`}>
+              <th scope="row">{a}</th>
               <td>{chance}</td>
             </tr>
           ))}
