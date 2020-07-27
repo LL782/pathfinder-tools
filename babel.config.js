0c64677998350bcd5178f7a62c897922277118c2
ll782 <laurence.lord@gmail.com>
2020-07-27 22:14:39 +0100

    Resolve issue with babel and styled JSX

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  plugins: ["@babel/plugin-transform-react-jsx"],
+  plugins: ["@babel/plugin-transform-react-jsx", "styled-jsx/babel"],
   presets: [["@babel/preset-env", { targets: { node: "current" } }]],
 };
