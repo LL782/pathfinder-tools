09b14883871d9e7604e1093f52b6903bba9057d3
ll782 <laurence.lord@gmail.com>
2021-06-18 21:46:46 +0100

    Improve header

--- Layout/Header.tsx
+++ Layout/Header.tsx
@@ -1,30 +1,54 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-export const Header = () => (
-  <>
-    <header>
-      <Link href="/random-alignment-tables">
-        <a>Random Alignment Tables</a>
-      </Link>
-      <Link href="/speed-and-distance">
-        <a>Speed and Distance</a>
-      </Link>
-    </header>
+export const Header = () => {
+  const pageLinks: Array<{ href: string; text: string }> = [
+    {
+      href: "/random-alignment-tables",
+      text: "Random Alignment Tables",
+    },
+    {
+      href: "/speed-and-distance",
+      text: "Speed and Distance",
+    },
+  ];
 
-    <style jsx>{`
-      header {
-        align-items: center;
-        display: flex;
-        flex-direction: row;
-        justify-content: left;
-      }
-      a {
-        padding: 0.5rem;
-      }
-      a:first-child {
-        padding-left: 0;
-      }
-    `}</style>
-  </>
-);
+  const currentPath = useRouter().asPath;
+
+  return (
+    <>
+      <header>
+        {pageLinks.map(({ href, text }) => {
+          const active = href === currentPath;
+
+          return (
+            <Link href={href} key={href}>
+              <a className={active && "active"}>{text}</a>
+            </Link>
+          );
+        })}
+      </header>
+
+      <style jsx>{`
+        header {
+          align-items: center;
+          display: flex;
+          flex-direction: row;
+          justify-content: left;
+        }
+        a {
+          padding: 0.5rem;
+        }
+        a:first-child {
+          padding-left: 0;
+        }
+        a.active {
+          color: black;
+          font-weight: bold;
+          text-decoration: none;
+        }
+      `}</style>
+    </>
+  );
+};
