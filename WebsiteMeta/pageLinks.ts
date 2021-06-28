0d60a1048e59295228b7e13a483acff91400c043
ll782 <laurence.lord@gmail.com>
2021-06-28 11:50:01 +0100

    Release hero stats generator

--- WebsiteMeta/pageLinks.ts
+++ WebsiteMeta/pageLinks.ts
@@ -3,11 +3,10 @@ export const pageLinks: Array<{ href: string; text: string }> = [
     href: "/",
     text: "Home",
   },
-  // Feature not ready for release
-  // {
-  //   href: "/hero-stats-generator",
-  //   text: "Hero Stats Generator",
-  // },
+  {
+    href: "/hero-stats-generator",
+    text: "Hero Stats Generator",
+  },
   {
     href: "/random-alignment-tables",
     text: "Random Alignment Tables",
